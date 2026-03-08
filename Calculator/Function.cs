46a98c977f4a786a8420625109ec435054217b01
Yarr2 <yarema.stus@gmail.com>
2026-03-08 22:55:11 +0200

    Added basic functionality of Function.cs

---
+++ Calculator/Function.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator;
+
+public class Function
+{
+    private int _numberOfArguments;
+    private string _name;
+
+    public double Calculate(double[] arguments)
+    {
+        if (arguments.Length != _numberOfArguments)
+        {
+            throw new Exception($"This function takes {_numberOfArguments} instead of {arguments.Length} given");
+        }
+
+        return arguments[0];
+    }
+}
