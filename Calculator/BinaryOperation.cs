afb547a79ccb0d73fa053c57b4de6759d9a7615a
Yarr2 <yarema.stus@gmail.com>
2026-03-09 16:52:38 +0200

    Added BinaryOperation.cs

---
+++ Calculator/BinaryOperation.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator;
+
+public class BinaryOperation
+{
+    public string Symbol;
+    public readonly int Preference;
+    public readonly string Assosiativity;
+    public readonly Func<double[], double> calculate;
+
+    public override string ToString()
+    {
+        return $"Operation '{Symbol}' with preference value {Preference},associativity - {Assosiativity}";
+    }
+}
