2f180015f0204c74121888af3eeb18f1de5bc9d3
Yarr2 <yarema.stus@gmail.com>
2026-03-09 16:55:59 +0200

    Added constructor to BinaryOperation.cs

--- Calculator/BinaryOperation.cs
+++ Calculator/BinaryOperation.cs
@@ -2,11 +2,18 @@
 
 public class BinaryOperation
 {
-    public string Symbol;
+    public readonly string Symbol;
     public readonly int Preference;
     public readonly string Assosiativity;
-    public readonly Func<double[], double> calculate;
+    public readonly Func<double[], double> _calculate;
 
+    public BinaryOperation(string symbol, int preference, string assosiativity, Func<double[],double> calculate)
+    {
+        Symbol = symbol;
+        Preference = preference;
+        Assosiativity = assosiativity;
+        _calculate = calculate;
+    }
     public override string ToString()
     {
         return $"Operation '{Symbol}' with preference value {Preference},associativity - {Assosiativity}";
