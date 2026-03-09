d0f80ab942b5baae9803426c5d2485395d555c03
Yarr2 <yarema.stus@gmail.com>
2026-03-09 17:30:46 +0200

    Added Tokenizer.cs

---
+++ Calculator/Tokenizer.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator;
+
+public class Tokenizer
+{
+    public static OwnQueue<string> Tokenize(string text, List<BinaryOperation> operations)
+    {
+        
+        OwnQueue<string> queue = new OwnQueue<string>(text.Length);
+        List<string> operationSymbols = operations.Select(operation => operation.Symbol).ToList();
+        string number = "";
+        foreach (char symbol in text)
+        {
+            if (symbol == ' ') continue;
+            if (symbol <= '9' && symbol >= '0')
+            {
+                number += symbol;
+                continue;
+            }
+            if ( operationSymbols.Contains(symbol.ToString()))
+            {
+                if (number != "") queue.Add(number);
+                queue.Add(symbol.ToString());
+                number = "";
+                continue;
+            }
+
+            throw new Exception("Our calculator does not support your symbols");
+        }
+
+        if (number != "") queue.Add(number);
+        return queue;
+    }
+}
