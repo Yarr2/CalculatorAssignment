78148c65a80e0c1ae537662a2359c65f778f922a
Yarr2 <yarema.stus@gmail.com>
2026-03-11 09:47:09 +0200

    Added Calculator.cs

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator;
+
+public class Calculator
+{
+    private OwnList<BinaryOperation> _operations;
+
+    public double CalculatePostFix(OwnQueue<string> postFix)
+    {
+        OwnStack<double> stack = new OwnStack<double>(postFix.Size);
+        while (postFix.Size != 0)
+        {
+            string value = postFix.Pop();
+            if (double.TryParse(value, out double result))
+            {
+                stack.Push(result);
+                continue;
+            }
+            _operations.
+            
+        }
+    }
+}
